c023fdaeded9aff79e40312d5dcd407a9ded3895
Soorria Saruva <soorria.ss@gmail.com>
2023-09-07 23:28:23 +1000

    add convex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import '~/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import ConvexClientProvider from '~/lib/convex'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
