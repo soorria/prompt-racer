b9f18f0fc45e361ad81bf74bd0bd28d0ebd4f9f8
Eric Paul <ericcpaul00@gmail.com>
2023-09-09 11:28:48 +1000

    fuxunb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,21 @@
 import { SignInButton, UserButton } from "@clerk/nextjs"
 import { useConvexAuth } from "convex/react"
 import { ModeToggle } from "~/components/ModeToggle"
+import NavBar from "~/components/NavBar"
 import { Button } from "~/components/ui/button"
+import { useUser } from "@clerk/nextjs"
 
 export default function Home() {
   const { isAuthenticated } = useConvexAuth()
+  const user = useUser()
+  console.log(user)
+
   return (
-    <main className="flex min-h-screen items-center justify-between p-24">
-      {isAuthenticated ? <UserButton afterSignOutUrl="/" /> : <SignInButton mode="modal" />}
-      <Button>Test</Button>
-      <ModeToggle />
-    </main>
+    <>
+      <NavBar />
+      <main className="flex min-h-screen items-center justify-between p-24">
+        {isAuthenticated ? <UserButton afterSignOutUrl="/" /> : <SignInButton mode="modal" />}
+      </main>
+    </>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Button } from "./ui/button"
+import { ModeToggle } from "./ModeToggle"
+import { Fugaz_One } from "next/font/google"
+
+const Fugaz = Fugaz_One({ weight: "400", subsets: ["latin"] })
+
+type Props = {}
+
+export default function NavBar({}: Props) {
+  return (
+    <div className="flex-row justify-between flex px-5 py-5 bg-card items-center">
+      <div className={"font-display"}>
+        <div className="text-xl flex flex-row">
+          PROMPT<div className="text-primary">RACER</div>
+        </div>
+      </div>
+      {/* <ModeToggle /> */}
+    </div>
+  )
+}
