5a5fcfddd0ac68d11ffafb9a2e2df56caccc6f0c
Eric Paul <ericcpaul00@gmail.com>
2023-09-09 10:30:31 +1000

    Finished added shadcn and lucide icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 import { SignInButton, UserButton } from "@clerk/nextjs"
 import { useConvexAuth } from "convex/react"
+import { ModeToggle } from "~/components/ModeToggle"
+import { Button } from "~/components/ui/button"
 
 export default function Home() {
   const { isAuthenticated } = useConvexAuth()
   return (
     <main className="flex min-h-screen items-center justify-between p-24">
       {isAuthenticated ? <UserButton afterSignOutUrl="/" /> : <SignInButton mode="modal" />}
+      <Button>Test</Button>
+      <ModeToggle />
     </main>
   )
 }
