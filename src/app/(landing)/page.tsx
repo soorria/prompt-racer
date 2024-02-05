9a30221be15bab85035c8e9e4b6ee94beceaeb75
Soorria Saruva <soorria.ss@gmail.com>
2024-02-05 22:02:10 +1100

    fix: prevent annoying overflow on landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <div className="h-full">
       <div className="isolate overflow-hidden">
-        <div
+        {/* TODO: re-add when it doesn't cause havoc on mobile */}
+        {/* <div
           className="absolute left-[calc(50%-4rem)] top-10 -z-50 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
           aria-hidden="true"
         >
@@ -16,7 +17,7 @@ export default function Home() {
                 "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
             }}
           />
-        </div>
+        </div> */}
         <div className="mx-auto max-w-7xl items-center px-6  pt-10 lg:flex lg:px-8">
           <div className="mx-auto pr-12 max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
             <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
