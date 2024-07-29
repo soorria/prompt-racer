2ab4347d6a0b5d3fab209421728943ac5b2cdd17
Soorria Saruva <soorria.ss@gmail.com>
2024-07-29 21:40:20 +1000

    feat: init

---
+++ src/server/db/schema.ts
@@ -0,0 +1,28 @@
+// Example model schema from the Drizzle docs
+// https://orm.drizzle.team/docs/sql-schema-declaration
+
+import { sql } from "drizzle-orm";
+import {
+  index,
+  serial,
+  timestamp,
+  varchar,
+  pgTable,
+} from "drizzle-orm/pg-core";
+
+export const posts = pgTable(
+  "post",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (example) => ({
+    nameIndex: index("name_idx").on(example.name),
+  }),
+);
