000000049496961198622047239ff52821a6f8ec
Soorria Saruva <soorria.ss@gmail.com>
2024-08-25 19:25:23 +1000

    chore: delete tmp stuff

--- src/app/tmp/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { env } from "~/env"
-
-export default function TmpLayout(props: { children: React.ReactNode }) {
-  if (env.NODE_ENV !== "development") return null
-
-  return <div>{props.children}</div>
-}
