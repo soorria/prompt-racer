50d32626e0284985af915e19a2d0930437dfa540
Eric Paul <ericcpaul00@gmail.com>
2024-08-29 19:49:09 +1000

    chore: update input limit

--- src/app/api/game/send-message/route.ts
+++ src/app/api/game/send-message/route.ts
@@ -4,7 +4,7 @@ import { sendMessageInGame } from "~/lib/games/internal-actions"
 
 const BodySchema = z.object({
   gameId: z.string(),
-  prompt: z.string().min(1).max(40),
+  prompt: z.string().min(1).max(50),
 })
 
 export const POST = async (request: Request) => {
