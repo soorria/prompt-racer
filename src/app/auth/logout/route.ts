0000000a5a13ad565e6033390f490979dc3620a0
Soorria Saruva <soorria.ss@gmail.com>
2024-09-08 20:13:19 +1000

    fix: stop warning during build

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation"
 
 import { logoutAction } from "~/lib/auth/actions"
 
+export const dynamic = "force-dynamic"
+
 export async function GET() {
   await logoutAction()
   redirect("/")
