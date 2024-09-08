031832ce18cb3f812c886f2c128f07a7bbbfe1ed
Eric Paul <ericcpaul00@gmail.com>
2024-09-08 21:24:52 +1000

    feat: new nav

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -31,9 +31,7 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
-    onClose?(): void
-  }
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & { onClose?: () => void }
 >(({ onClose, className, children, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay onClick={onClose} />
