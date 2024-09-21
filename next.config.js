00000002dfd3630728df14dd8960700ecee90667
Soorria Saruva <soorria.ss@gmail.com>
2024-09-21 16:36:51 +1000

    feat: try ppr

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import("./src/env.js")
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -14,7 +14,8 @@ const config = {
   },
   experimental: {
     typedRoutes: true,
+    ppr: true,
   },
-};
+}
 
-export default config;
+export default config
