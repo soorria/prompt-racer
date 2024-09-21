06d2735ede7b6785928ebbdb15c9eff4992865d0
Soorria Saruva <soorria.ss@gmail.com>
2024-09-21 16:36:51 +1000

    feat: try ppr

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,19 +1,22 @@
-import React from "react"
+import React, { Suspense } from "react"
 
 import Logo from "~/components/nav-bar/Logo"
-import ProfileCard from "~/components/nav-bar/ProfileCard"
-import { getAuthUser, getDBUser } from "~/lib/auth/user"
+import { ProfileCard } from "~/components/nav-bar/ProfileCard"
 import Navbar from "~/lib/surfaces/navbar/Navbar"
 import { TRPCReactProvider } from "~/lib/trpc/react"
 
 export default async function BaseLayoutWithNavbar({ children }: { children: React.ReactNode }) {
-  const user = await getAuthUser()
-  const dbUser = user ? await getDBUser(user.id) : null
-
   return (
     <TRPCReactProvider>
       <div>
-        <Navbar leftContent={<Logo />} rightContent={<ProfileCard user={dbUser} />} />
+        <Navbar
+          leftContent={<Logo />}
+          rightContent={
+            <Suspense>
+              <ProfileCard />
+            </Suspense>
+          }
+        />
         {children}
       </div>
     </TRPCReactProvider>
--- src/components/nav-bar/ProfileCard.tsx
+++ src/components/nav-bar/ProfileCard.tsx
@@ -1,36 +1,21 @@
-"use client"
+import { Suspense } from "react"
+import { unstable_noStore } from "next/cache"
 
-import React from "react"
-import Link from "next/link"
-import { BookOpen } from "lucide-react"
-
-import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover"
-import { type Doc } from "~/lib/db/types"
-import { cn } from "~/lib/utils"
-import { Button } from "../ui/button"
-import LoginLogoutButton from "./LoginLogoutButton"
+import { getAuthUser, getDBUser } from "~/lib/auth/user"
+import ClientProfileCard from "./ProfileCard.client"
 import UserAvatar from "./UserAvatar"
 
-export default function ProfileCard({ user }: { user: Doc<"users"> | null | undefined }) {
-  const [open, setOpen] = React.useState(false)
+async function ProfileCardImpl() {
+  unstable_noStore()
+  const user = await getAuthUser()
+  const dbUser = user ? await getDBUser(user.id) : null
+  return <ClientProfileCard user={dbUser} />
+}
 
+export function ProfileCard() {
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger className={cn("flex items-center justify-center space-x-3 rounded-full")}>
-        <UserAvatar name={user?.name} imageUrl={user?.profile_image_url} />
-      </PopoverTrigger>
-      <PopoverContent className="p-0 py-2" align="end">
-        <LoginLogoutButton key={user?.id ?? ""} isAuthenticated={!!user} setOpen={setOpen} />
-        <div className="spacer my-2 h-0.5 w-full bg-gray-200/20" />
-        <Button
-          asChild
-          variant={"ghost"}
-          Icon={BookOpen}
-          className="w-full justify-start rounded-none"
-        >
-          <Link href="/auth/login">Privacy Policy</Link>
-        </Button>
-      </PopoverContent>
-    </Popover>
+    <Suspense fallback={<UserAvatar name={undefined} imageUrl={undefined} />}>
+      <ProfileCardImpl />
+    </Suspense>
   )
 }
