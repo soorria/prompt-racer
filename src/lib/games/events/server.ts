1a24be9b712b502cac573787658bc28b5009d548
Soorria Saruva <soorria.ss@gmail.com>
2025-01-02 00:39:31 +1100

    feat: make mega-explicit when you send an event to all users

--- src/lib/games/events/server.ts
+++ src/lib/games/events/server.ts
@@ -4,13 +4,15 @@ import { schema } from "~/lib/db"
 import { type DBOrTransation } from "~/lib/db/types"
 import { type GameEvent } from "./schema"
 
+const ALL_USERS: unique symbol = Symbol("push-event:all-users")
 export async function pushEvent(
   db: DBOrTransation,
-  args: { gameId: string; event: GameEvent; userId: string | null },
+  args: { gameId: string; event: GameEvent; userId: string | typeof ALL_USERS },
 ) {
   await db.insert(schema.gameEvents).values({
     game_id: args.gameId,
     payload: args.event,
-    user_id: args.userId,
+    user_id: args.userId === ALL_USERS ? null : args.userId,
   })
 }
+pushEvent.ALL_USERS = ALL_USERS
