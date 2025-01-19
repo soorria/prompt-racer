0000000b0fb1b47cdbb0c9e91a319d9265330236
Soorria Saruva <soorria.ss@gmail.com>
2025-01-19 16:42:28 +1100

    fix: actually enable event cleanup

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,9 +1,10 @@
 import { serve } from "inngest/next"
 
 import { inngest } from "~/lib/inngest/client"
+import { cleanupGameEventsFunction } from "~/lib/inngest/functions/cleanup-game-events"
 import { gameWorkflowFunction } from "~/lib/inngest/functions/game-workflow"
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [gameWorkflowFunction],
+  functions: [gameWorkflowFunction, cleanupGameEventsFunction],
 })
